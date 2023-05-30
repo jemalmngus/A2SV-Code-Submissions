5d770564b6ef886f43e23a63d8bc470838107a38
jemal <jemalmngus@gmail.com>
2023-05-30 20:42:00 +0300

    Define  multiplication of strings

---
+++ string-challenges/MultiplyString.py
@@ -0,0 +1,19 @@
+# Problem: Multiply Strings
+# Link: https://leetcode.com/problems/multiply-strings/
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+
+        # The following solution converts strings to integers, which is not allowed
+        # according to the problem instructions
+        # return str(int(num1) * int(num2))
+        
+        # TODO: Implement the appropriate solution here
+        
+        pass
+
+# sample test case    
+num1="2"
+num2="3"
+solution = Solution()
+print(solution.multiply_strings(num1, num2)) #Output "6"
